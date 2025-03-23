5df1a9dc175f09bfecafe9c8ebc809e92c36e645
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-03-23 15:47:13 +0100

    c7

--- app/src/Dbconnexion/Connexion.java
+++ app/src/Dbconnexion/Connexion.java
@@ -1,5 +1,21 @@
 package Dbconnexion;
 
-public class Connexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Connexion {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/projetjava";
+        String username = "root";
+        String password = "bassem1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        try (connection) {
+            System.out.println("Connected Succesfully!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
 }
